3aa31175a6ab10e03ed93eed5721463bd1789a8b
juan <juanmonsalveh@gmail.com>
2015-06-08 17:58:48 -0500

    Creación y consumo del primer Web Service (hola <param>)

---
+++ AndroidWSClient/app/src/main/java/com/example/androidwsclient/AndroidWSClientActivity.java
@@ -0,0 +1,73 @@
+package com.example.androidwsclient;
+
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapPrimitive;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransportSE;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidWSClientActivity extends Activity {
+
+    private static final String SOAP_ACTION = "http://www.spopatest.unal.edu.co/CompanyMobileServicesService/hello";
+    private static final String METHOD_NAME = "hello";
+    private static final String NAMESPACE = "http://facadeWS.spopa.unal.dev/";
+    private static final String URL = "http://www.spopatest.unal.edu.co/CompanyMobileServicesService/CompanyMobileServicesService?wsdl";
+
+    Button hola;//holaButton
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_wsclient);
+
+
+        hola = (Button) findViewById(R.id.holaButton);
+
+        hola.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        Thread networkThread = new Thread() {
+                                            @Override
+                                            public void run() {
+                                                try {
+
+                                                    String name = "Juan";
+
+                                                    SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME);
+                                                    request.addProperty("name", name);
+                                                    SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+                                                    envelope.setOutputSoapObject(request);
+
+                                                    HttpTransportSE ht = new HttpTransportSE(URL);
+                                                    ht.call(SOAP_ACTION, envelope);
+                                                    final SoapPrimitive response = (SoapPrimitive) envelope.getResponse();
+                                                    final String str = response.toString();
+
+                                                    runOnUiThread(new Runnable() {
+                                                        public void run() {
+                                                            TextView result;
+                                                            result = (TextView) findViewById(R.id.textView1);//Text view id is textView1
+                                                            result.setText(str);
+                                                        }
+                                                    });
+                                                } catch (Exception e) {
+                                                    e.printStackTrace();
+                                                }
+                                            }
+                                        };
+                                        networkThread.start();
+                                    }
+                                }
+
+        );
+    }
+}
